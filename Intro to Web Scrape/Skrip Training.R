624478dd34b305ecce968dc91711bfe9f51f24d1
ikanx101 <ikanx101@gmail.com>
2022-08-08 10:43:05 +0700

    sace

--- Intro to Web Scrape/Skrip Training.R
+++ Intro to Web Scrape/Skrip Training.R
@@ -7,10 +7,15 @@
 # untuk membersihkan global environment
 rm(list=ls())
 
+# untuk install libraries
+# install.packages("rvest")
+# install.packages("dplyr")
+
 # libraries parsing html
 library(dplyr) # berguna untuk data carpentry / data manipulation
 library(rvest) # berguna untuk parsing html
 
-# untuk install libraries
-install.packages("rvest")
-install.packages("dplyr")
+# dplyr ini adalah satu bagian dari yg namanya tidyverse --> dplyr, plyr, rvest, tidymodels, tidytext
+# di R, itu ada dua aliran (mazhab) untuk melakukan data carpentry:
+  # 1. prinsip tidy --> %>% (ini dibacanya "then...") 
+  # 2. prinsip data.table --> function_1(function_2(function_3()))
