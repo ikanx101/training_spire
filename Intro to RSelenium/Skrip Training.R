665db3db21f3570c32f261493d648ac15200f4be
ikanx101 <ikanx101@gmail.com>
2022-09-21 08:50:54 +0700

    update

---
+++ Intro to RSelenium/Skrip Training.R
@@ -0,0 +1,12 @@
+# ===============================================================================
+# intro to RSelenium
+#
+# ikanx101.com
+#
+#
+# pastikan sudah install library RSelenium terlebih dahulu ya
+#
+# ===============================================================================
+
+# cara instalasi:
+install.packages("RSelenium")
