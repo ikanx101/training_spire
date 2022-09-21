efe72c6313a642c67668713f472b02e6917eac75
ikanx101 <ikanx101@gmail.com>
2022-09-21 09:18:38 +0700

    update

--- Sesi 3 Intro to RSelenium/Skrip Training.R
+++ Sesi 3 Intro to RSelenium/Skrip Training.R
@@ -9,4 +9,27 @@
 # ===============================================================================
 
 # cara instalasi:
-install.packages("RSelenium")
+# install.packages("RSelenium")
+
+
+
+
+# ===============================================================================
+# RSelenium merupakan metode mimicking browser sehingga sangat ampuh untuk digunakan 
+  # untuk melakukan web scraping dari situs manapun.
+  # Kali ini kita akan melakukan web scraping dari situs grab food.
+  # Mari kita mulai...
+
+# kita set working directory
+setwd("~/Documents/Training SPIRE/Sesi 3 Intro to RSelenium")
+
+# kita hapus global environment
+rm(list=ls())
+
+# memanggil libraries
+library(dplyr)        # digunakan agar kita bisa menggunakan ` %>% `
+library(rvest)        # digunakan untuk melakukan parsing html
+library(RSelenium)    # digunakan untuk memanggil RSelenium
+
+# set url grab
+url = "https://food.grab.com/id/id/"
